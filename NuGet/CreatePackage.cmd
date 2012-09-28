337dce9643061f69e9ff6b988147ff68fa84635b
Serge van den Oever <serge@macaw.nl>
2012-09-28 13:43:32 +0200

    cleanup of merged files

--- NuGet/CreatePackage.cmd
+++
@@ -1,2 +0,0 @@
-%~dp0NuGet.exe Pack WebMatrix.Executer.nuspec -Verbose -BasePath %~dp0 -OutputDirectory %~dp0..\NuGetPackages
-pause
--- NuGet/PublishToPublicNuGet.cmd
+++
@@ -1,3 +0,0 @@
-%~dp0NuGet.exe setApiKey 146eece3-3b6e-4263-a66c-4c5220df692e
-%~dp0NuGet.exe push %~dp0..\NuGetPackages\WebMatrix.Executer.1.0.0.0.nupkg
-pause
