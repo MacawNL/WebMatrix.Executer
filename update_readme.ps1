4c69aa7b967820b806745eb4495e027c220bf79a
Serge van den Oever <serge@macaw.nl>
2012-10-02 00:23:56 +0200

    Updated the documentation

---
+++ update_readme.ps1
@@ -0,0 +1,14 @@
+$executingScriptDirectory = Split-Path -Path $MyInvocation.MyCommand.Definition -Parent
+pushd $executingScriptDirectory
+git add Readme.md
+git commit -m "Updated the documentation"
+git push
+[System.Diagnostics.Process]::Start("https://github.com/MacawNL/WebMatrix.Executer/blob/master/Readme.md")
+$wc = New-Object System.Net.WebClient
+$wc.DownloadString("http://documentup.com/MacawNL/WebMatrix.Executer") > $executingScriptDirectory\WebMatrix.Executer.gh-pages\WebMatrix.Executer\index.html
+cd $executingScriptDirectory\WebMatrix.Executer.gh-pages\WebMatrix.Executer
+git add index.html
+git commit -m "Updated DocumentUp version of Readme.rd"
+git push
+popd
+[System.Diagnostics.Process]::Start("http://macawnl.github.com/WebMatrix.Executer/")
